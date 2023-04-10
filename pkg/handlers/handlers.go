a2c94fc8ae388994a9d270ac8c89eaf493e7dd83
UO <urazmakhatovatabek@mail.ru>
2023-04-10 13:43:10 +0300

    Initial commit

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,53 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/i5050i100it/bookings/pkg/config"
+	"github.com/i5050i100it/bookings/pkg/models"
+	"github.com/i5050i100it/bookings/pkg/render"
+)
+
+// Repo the repository ised biy the handlers
+var Repo *Repository
+
+// Repository is a repositury type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlelrs sets the Repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the home page
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+// About is the about page
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	//performe some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello , again."
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+
+	stringMap["remote_ip"] = remoteIP
+
+	//send the data to the template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
